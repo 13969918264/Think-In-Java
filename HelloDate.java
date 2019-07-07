c90ef69551712e70f8cfb6bd2e08903e71413134
赵子豪 <13969918264@163.com>
2019-07-07 20:36:19 +0800

    Create HelloDate.java

---
+++ HelloDate.java
@@ -0,0 +1,10 @@
+//:	HelloDate.java
+import java.util.*;
+import static net.mindview.util.Print.*;
+
+public class HelloDate {
+	public static void main(String[] args) {
+		print("hello. it's :  ");
+		print(new Date());
+	}
+}
